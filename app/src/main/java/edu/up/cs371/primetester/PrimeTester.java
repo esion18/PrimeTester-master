335bc967c4ef8246922f28b546eaf379bd825933
Emmanuel Sion <sion18@up.edu>
2017-09-02 17:08:23 -0700

    Changed the isPrime method. Added a LogCat to keep track of variable "i" in the "isPrime" method. Added sleeping_kitten.jpg to "res/mipmap" folder.

---
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -0,0 +1,38 @@
+package edu.up.cs371.primetester;
+
+import android.util.Log;
+
+/**
+ * Created by vegdahl on 7/17/2015.
+ *
+ * The purpose of this class is to define a static method, isPrime, that determines whether an
+ * integer is a prime number.
+ */
+public class PrimeTester {
+    /**
+     * Tells whether an integer is prime.
+     *
+     * @param n the number to test
+     * @return true iff n is prime
+     */
+    public static boolean isPrime(long n) {
+        // for now, return a random result
+        if(n <=1) {
+            return false;
+        }
+        else if(n <= 3){
+            return true;
+        }
+        else if(n % 2 == 0 || n % 3 ==0){
+            return false;
+        }
+
+        for(int i = 2; i<n; i++){
+            Log.i("isPrime", "Value of i:"+i);
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
